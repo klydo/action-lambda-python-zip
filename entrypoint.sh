71da2fb182594bce7a71a3003e29db260ab5ca92
Wai-chuen <wai-chuen@klydo.ai>
2020-07-10 12:39:28 +0100

    Finalised script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 
-configure_aws_credentials(){
-	aws configure set aws_access_key_id "${INPUT_AWS_ACCESS_KEY_ID}"
-    aws configure set aws_secret_access_key "${INPUT_AWS_SECRET_ACCESS_KEY}"
-    aws configure set default.region "${INPUT_LAMBDA_REGION}"
+parse_yaml(){
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=%s\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+does_lambda_exist() {
+  aws lambda get-function --function-name $1 > /dev/null 2>&1
+  if [ 0 -eq $? ]; then
+    true
+  else
+    false
+  fi
 }
 
 install_zip_dependencies(){
-	echo "Installing and zipping dependencies..."
+	echo "${LAMBDA_FUNCTION_NAME} Installing and zipping dependencies..."
 	mkdir python
-	pip install --target=python -r "${INPUT_REQUIREMENTS_TXT}"
+    pipenv lock --requirements >> requirements.txt
+	pip install --target=python -qr requirements.txt
 	if [ "${INPUT_EXCLUDE_BOTOCORE}" == "true" ]; then
 		rm -rf ./python/botocore*
 	fi
-	zip -r dependencies.zip ./python
+	zip -qr dependencies.zip ./python
 	zipsplit -n 50000000 dependencies.zip
 	rm dependencies.zip
+    rm requirements.txt
+	rm -rf python
 }
 
 process_dependencies() {
+	echo "${LAMBDA_FUNCTION_NAME} process_dependencies..."
 	FILES=depende*.zip
 	for f in $FILES
 	do
@@ -29,37 +53,102 @@ process_dependencies() {
 }
 
 publish_dependencies_as_layer(){
+	echo "${LAMBDA_FUNCTION_NAME} publish_dependencies_as_layer..."
     echo ""
 	FILE_NAME=$1
 	echo "Publishing $FILE_NAME as a layer..."
 	FILE_NUMBER=${FILE_NAME//[^0-9]/}
 	echo "FILE_NUMBER: $FILE_NUMBER"
-	LAYER_NAME="${INPUT_LAMBDA_LAYER_ARN}${FILE_NUMBER}"
+	LAYER_NAME="${LAMBDA_FUNCTION_NAME}-${FILE_NUMBER}"
 	echo "LAYER_NAME: $LAYER_NAME"
 	local result=$(aws lambda publish-layer-version --layer-name "${LAYER_NAME}" --zip-file fileb://${FILE_NAME})
-	LAYER_VERSION=$(jq '.Version' <<< "$result")
-	ALL_LAMBDA_LAYERS="${ALL_LAMBDA_LAYERS} ${LAYER_NAME}:${LAYER_VERSION}"
+	LAYER_VERSION_ARN=$(jq -r '.LayerVersionArn' <<< "$result")
+	ALL_LAMBDA_LAYERS="${ALL_LAMBDA_LAYERS} ${LAYER_VERSION_ARN}"
 	echo $ALL_LAMBDA_LAYERS
 	rm ${FILE_NAME}
 }
 
-publish_function_code(){
-	echo "Deploying the code itself..."
-	zip -r code.zip . -x \*.git\*
-	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
+create_or_update_function_code(){
+	echo "${LAMBDA_FUNCTION_NAME} Creating/Deploying the code itself..."
+
+    does_lambda_exist $LAMBDA_FUNCTION_NAME
+    status=$?
+    if ! $(exit $status);
+    then
+        echo "Creating $LAMBDA_FUNCTION_NAME"
+        aws lambda create-function --function-name $LAMBDA_FUNCTION_NAME --runtime $settings_runtime --role $settings_role --handler $settings_handler --zip-file fileb://code.zip
+    else
+        echo "Updating $LAMBDA_FUNCTION_NAME"
+	    aws lambda update-function-code --function-name "${LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
+    fi
 }
 
-update_function_layers(){
-	echo "Using the layer in the function..."
-	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers ${ALL_LAMBDA_LAYERS}
+update_lambda_configuration() {
+    aws lambda update-function-configuration --function-name $LAMBDA_FUNCTION_NAME --layers ${ALL_LAMBDA_LAYERS} --runtime $settings_runtime --role $settings_role --handler $settings_handler --timeout $settings_timeout --memory-size $settings_memory
 }
+
+configure_aws_credentials(){
+	aws configure set aws_access_key_id "${INPUT_AWS_ACCESS_KEY_ID}"
+    aws configure set aws_secret_access_key "${INPUT_AWS_SECRET_ACCESS_KEY}"
+    aws configure set default.region "${INPUT_AWS_REGION}"
+}
+
+generate_function_name(){
+    LAMBDA_FUNCTION_NAME="${INPUT_LAMBDA_FUNCTION_PREFIX}"
+    if [ ! -z "$INPUT_LAMBDA_FUNCTION_SUFFIX" ]
+    then
+        LAMBDA_FUNCTION_NAME="${LAMBDA_FUNCTION_NAME}-${INPUT_LAMBDA_FUNCTION_SUFFIX}"
+    fi
+    LAMBDA_FUNCTION_NAME="${LAMBDA_FUNCTION_NAME}-$1"
+}
+
+process_lambda_config(){
+    echo ""
+    echo "Parsing $1config.yml"
+    eval $(parse_yaml $1config.yml)
+
+    # echo $entrypoint
+    generate_function_name $entrypoint
+    echo "Function: ${LAMBDA_FUNCTION_NAME}"
+    
+    # temporarily change working dir
+    pushd $1
+
+    # generate requirements.txt from Pipfile
+    # pip install to ./python dir
+    # zip dependencies
+    # split into 50mb archives
+    install_zip_dependencies
+
+    # loop through all split archives
+    # and push as layers
+    process_dependencies
+
+    # reset working directory
+    popd
+
+    # create or update function code
+    create_or_update_function_code
+
+    # update lambda config with new layers and settings from config.yml
+    update_lambda_configuration
+
+}
+
+get_function_configs(){
+	FUNCTION_DIRS=${INPUT_LAMBDA_CONFIGS_PATH}*/
+	for f in $FUNCTION_DIRS
+	do
+        process_lambda_config $f
+	done
+}
+
 
 deploy_lambda_function(){
     configure_aws_credentials
-	install_zip_dependencies
-	process_dependencies
-	publish_function_code
-	update_function_layers
+	zip -r code.zip . -x \*.*/\*
+    get_function_configs
+    rm code.zip
 }
 
 deploy_lambda_function
